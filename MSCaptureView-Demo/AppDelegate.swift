c451f7be31f283b4ec0bbb06232936c995f87ab4
Steve Sheets <magesteve@mac.com>
2020-08-15 20:17:43 -0400

    Initial Commit

---
+++ MSCaptureView-Demo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MSCaptureView-Demo
+//
+//  Created by Steve Sheets on 8/15/20.
+//  Copyright © 2020 Steve Sheets. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MSCaptureView-Demo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MSCaptureView-Demo
+//
+//  Created by Steve Sheets on 8/15/20.
+//  Copyright © 2020 Steve Sheets. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
